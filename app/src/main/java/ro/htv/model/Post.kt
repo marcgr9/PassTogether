a25cf71da3a521db6d5dc52fe08b8112763a5707
Marc <marcgruita@gmail.com>
2020-04-13 11:35:51 +0300

    get list of posts in postaritopic

---
+++ app/src/main/java/ro/htv/model/Post.kt
@@ -0,0 +1,12 @@
+package ro.htv.model
+
+data class Post(
+        var ownwer_uid: String = "",
+        var owner_name: String = "",
+        var topic: String = "",
+        var text: String = "",
+        var timestamp: String = "",
+        var linkToImage: String = "",
+        var post: Boolean = true,
+        var parent: String = ""
+)
