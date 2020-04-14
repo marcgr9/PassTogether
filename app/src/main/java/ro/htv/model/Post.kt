56bc40c68e997ef1a1c4f944a9351771b301e809
Marc <marcgruita@gmail.com>
2020-04-14 21:53:42 +0300

    karma system

--- app/src/main/java/ro/htv/model/Post.kt
+++ app/src/main/java/ro/htv/model/Post.kt
@@ -7,6 +7,7 @@ data class Post(
         var ownwer_uid: String = "",
         var owner_name: String = "",
         var owner_profilePicture: String = Utils.defaultProfilePicture,
+        var owner_karma: Int = 0,
         var topic: String = "",
         var text: String = "",
         var timestamp: String = "",
