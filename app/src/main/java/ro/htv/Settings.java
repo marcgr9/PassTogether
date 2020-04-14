895b4626ee786d08c656d81fc96fb858e879693c
Marc <marcgruita@gmail.com>
2020-04-14 09:53:11 +0300

    settings page fixed

--- app/src/main/java/ro/htv/Settings.java
+++ app/src/main/java/ro/htv/Settings.java
@@ -1,24 +1,15 @@
 package ro.htv;
 
-import android.app.Dialog;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.bumptech.glide.Glide;
-import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
@@ -28,7 +19,6 @@ import ro.htv.model.Response;
 import ro.htv.model.User;
 import ro.htv.utils.AuthRepository;
 import ro.htv.utils.FirestoreRepository;
-import ro.htv.utils.StorageRepository;
 import ro.htv.utils.Utils;
 
 public class Settings extends AppCompatActivity {
@@ -42,15 +32,10 @@ public class Settings extends AppCompatActivity {
     private RecyclerView.LayoutManager layoutManager;
 
     private String uid = "";
-    private String topic = "";
-    private String userProfileImage = Utils.defaultProfilePicture;
 
     private FirestoreRepository firestoreRepository;
-    private StorageRepository storageRepository;
 
-    //private Post post = new Post();
-
-    //private Dialog addPost;
+    private User user;
 
     private ArrayList<Post> posts = new ArrayList<>();
 
@@ -62,25 +47,31 @@ public class Settings extends AppCompatActivity {
 
         logout_btn = findViewById(R.id.LogOutButton);
         profile_btn = findViewById(R.id.ProfileButton);
+
         uid = getIntent().getStringExtra("uid");
-        topic = "Concurs UBB Mate";
         firestoreRepository = new FirestoreRepository();
-        storageRepository = new StorageRepository();
 
         MutableLiveData<Response> userData = firestoreRepository.getUser(uid);
+        userData.observe(this, new Observer<Response>() {
+            @Override
+            public void onChanged(Response response) {
+                if (response.ok()) {
+                    user = (User) response.getValue();
+                    loadPosts();
+                }
+            }
+        });
+
 
         recyclerView = findViewById(R.id.review);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
-        loadPosts();
 
         logout_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AuthRepository as = new AuthRepository();
                 as.logout();
-                /// sau FirebaseAuth.getInstance().signOut();
-                //                finish();
                 startActivity(new Intent(Settings.this, Login.class));
             }
         });
@@ -94,26 +85,19 @@ public class Settings extends AppCompatActivity {
     }
 
         private void loadPosts() {
-            FirestoreRepository fs = new FirestoreRepository();
-            MutableLiveData<PostsResponse> postsReq = fs.getPostsByUser(uid);
+            MutableLiveData<PostsResponse> postsReq = firestoreRepository.getPostsByUser(uid);
 
             postsReq.observe(this, new Observer<PostsResponse>() {
                 @Override
                 public void onChanged(PostsResponse postsResponse) {
                     if (postsResponse.getStatus() == Utils.Responses.OK) {
-                        ArrayList<Postare> lista = new ArrayList<>();
-
                         posts = postsResponse.getPosts();
-                        for(Post X: posts)
-                        {
-                            lista.add(new Postare(X.getLinkToImage(),X.getLinkToImage(),X.getOwner_name(), X.getText(), 1));
-                        }
+                        adapter = new AdapterList(posts);
 
-                        adapter = new AdapterList(lista);
                         adapter.setOnItemClick(new AdapterList.OnItemClickListener() {
                             @Override
                             public void OnItemClick(int poz) {
-                                startActivity(new Intent(getBaseContext(), CometariiPostare.class));
+                                startActivity(new Intent(getBaseContext(), CometariiPostare.class).putExtra("idPost", posts.get(poz).getIdpost()).putExtra("profileImage", user.getProfileImage()).putExtra("uid", uid).putExtra("currentUserName", user.getName()));
                             }
                         });
 
